96881db303bc049ef367c17f2cc44340c257a17e
dbescond <bescond@ilo.org>
2021-02-10 12:26:31 +0100

    add version 1.2.0

--- R/runExplorer.R
+++ R/runExplorer.R
@@ -2,6 +2,12 @@
 #'
 #' @description open ilostat explorer app on your computer.
 #'
+#' @param display.mode The mode in which to display the application. 
+#' If set to the value "showcase", shows application code and metadata 
+#' from a DESCRIPTION file in the application directory alongside the application. 
+#' If set to "normal", displays the application normally. Defaults to "auto", which 
+#' displays the application in the mode given in its DESCRIPTION file, if any.
+#'
 #' @name runExplorer
 #' @author David Bescond \email{bescond@ilo.org}
 #' @references
@@ -21,20 +27,18 @@ runExplorer <- function(display.mode = "normal") {
 	Sys.setenv(ftp_proxy="proxyos.ilo.org:8080") 
   }
   
-  myGlobal 	<- readLines(url("ftp://ilostatRO:1l0st4tR0@ftp.ilo.org/Rilostat/opensource/bulkexplorer/bulkexplorer_GLOBAL.r", method = 'libcurl', open = 'r'))
-  myUi 		<- readLines(url("ftp://ilostatRO:1l0st4tR0@ftp.ilo.org/Rilostat/opensource/bulkexplorer/bulkexplorer_UI.r", method = 'libcurl', open = 'r'))
-  myServer 	<- readLines(url("ftp://ilostatRO:1l0st4tR0@ftp.ilo.org/Rilostat/opensource/bulkexplorer/bulkexplorer_SERVER.r", method = 'libcurl', open = 'r'))
-  closeAllConnections()
+  myGlobal 	<- read_lines("ftp://ilostatRO:1l0st4tR0@ftp.ilo.org/Rilostat/opensource/bulkexplorer/bulkexplorer_GLOBAL.r")
+  myUi 		<- read_lines("ftp://ilostatRO:1l0st4tR0@ftp.ilo.org/Rilostat/opensource/bulkexplorer/bulkexplorer_UI.r")
+  myServer 	<- read_lines("ftp://ilostatRO:1l0st4tR0@ftp.ilo.org/Rilostat/opensource/bulkexplorer/bulkexplorer_SERVER.r")
   
-   
+
   shiny::runApp( list(
 				global = eval(parse(text =myGlobal)), 
 				ui = function(){eval(parse(text =myUi))}, 
 				server = function(input, output, session) {eval(parse(text= myServer))}
 				) 
 				, display.mode = display.mode)
-   closeAllConnections()
- 
+   
 
 				
 }
--- cran-comments.md
+++
@@ -1,2 +0,0 @@
-
-## notes on url solves
